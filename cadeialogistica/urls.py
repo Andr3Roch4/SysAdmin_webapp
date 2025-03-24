c0e393a061c7fcda78006f912ee28cb56ad2bee5
André Rocha <andre.rcrocha@gmail.com>
2025-03-24 21:36:34 +0000

    fixed urls

--- cadeialogistica/urls.py
+++ cadeialogistica/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("/encomendar", views.encomenda, name="encomenda"),
-    path("/comparar", views.comparar, name="comparar"),
-    path("/ideal", views.ideal, name="ideal"),
-    path("/criar", views.criar, name="criar"),
+    path("encomendar", views.encomenda, name="encomenda"),
+    path("comparar", views.comparar, name="comparar"),
+    path("ideal", views.ideal, name="ideal"),
+    path("criar", views.criar, name="criar"),
 ]
