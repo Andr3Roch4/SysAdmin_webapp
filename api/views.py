950d4e85b30b4e4e1f7eccbe5a8b1c18e82ea3ad
wmdantas <walter.geologia@example.com>
2025-03-27 19:15:12 +0000

    Variáveis para listar todas as classes da BD

--- api/views.py
+++ api/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Produto, Distribuidor, Fornecedor, Transportador
 from django.core import serializers
+from django.db.models import Q 
 import csv
 
 # Create your views here.
 
 def listar(request):
-    data=Produto.objects.all()                                      # Variavel data com todos os Produtos
+    p=Produto.objects.all()                                         # Variavel data com todos os Produtos
+    d=Distribuidor.objects.all()                                    # Variavel data com todos os Distribuidores
+    f=Fornecedor.objects.all()                                      # Variavel data com todos os Fornecedores
+    t=Transportador.objects.all()                                   # Variavel data com todos os Transportadores
+    data = list(p) + list(d) + list(f) + list(t)                                      
     json_data=serializers.serialize("json", data)                   # Transforma a data em json
     return HttpResponse(json_data, content_type='application/json') # Como já temos a data em json, temos de fazer Httpresponse e não JsonResponse
 
